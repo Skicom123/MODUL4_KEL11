8cedbf00e1a17a6bb2a59efde91122328144aa31
Skicom123 <57831982+Skicom123@users.noreply.github.com>
2020-04-23 08:56:03 +0700

    Update PerhitunganAnak.java

--- src/modul4_kel11/PerhitunganAnak.java
+++ src/modul4_kel11/PerhitunganAnak.java
@@ -8,10 +8,10 @@ public class PerhitunganAnak {
         System.out.println("Masukkan Usia Anak:");
         System.out.print("Tahun: ");
         float tahun = inputUsia.nextFloat();
-        System.out.print("Bulan: ");
+        System.out.print("Bulan: "); 
         float bulan = inputUsia.nextFloat();
         float hasil_anak = (float) (2*(tahun+(bulan*0.1))+8);
         System.out.println("BMI anak: " + hasil_anak);
-        return hasil_anak;
+        return hasil_anak; /** nanti hasil anak keluar dari output */
     }
 }
